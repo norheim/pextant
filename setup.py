2c49d51ec87a6b387ac64ddea4d86b1e6206e608
kezilu <kezilu@mit.edu>
2015-08-03 02:02:22 -0500

    Minor edits for new distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='Pextant',
-    version='0.1.1',
+    version='0.2.0',
     author='Kevin Lu',
     author_email='kezilu@mit.edu',
     packages=['pextant'],
