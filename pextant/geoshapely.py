f17b75cd693639077f6b7bebf0418a718f874091
norheim <norheim@mit.edu>
2016-11-08 19:58:37 -1000

    Added a bunch of helper tools

--- pextant/geoshapely.py
+++ pextant/geoshapely.py
@@ -85,8 +85,19 @@ class Cartesian(GeoType):
     def post_process(self, transformed_points):
         points_easting, points_northing = transformed_points
         x, y = (points_easting - self.origin_easting, self.origin_northing - points_northing)
-        return np.array([np.floor(x / self.resolution), np.floor(y / self.resolution)]).astype(int)
+        return np.array([np.round(x / self.resolution,6), np.round(y / self.resolution,6)]).astype(int)
 
+class Row_Col(Cartesian):
+    def __init__(self, origin, resolution):
+        Cartesian.__init__(self, origin, resolution)
+
+    def getargs(self, geo_points):
+        arg1, arg2 = Cartesian.getargs(self, geo_points)
+        return arg2, arg1
+
+    def post_process(self, transformed_points):
+        arg1, arg2 = Cartesian.post_process(self, transformed_points)
+        return arg2, arg1
 
 class GeoObject(object):
     def __init__(self, geo_type, x, y):
@@ -105,6 +116,9 @@ class GeoObject(object):
         # assuming other_reference is not of type utm
         return self.original_reference.transform(self.data, other_reference)
 
+    def eastingnorthing(self):
+        return self.easting, self.northing
+
 
 class GeoPoint(GeoObject, Point):
     def __init__(self, geo_type, x, y):
@@ -113,12 +127,48 @@ class GeoPoint(GeoObject, Point):
 
 
 class GeoPolygon(GeoObject, LineString):
-    def __init__(self, geo_type, x, y):
-        GeoObject.__init__(self, geo_type, x, y)
+    def __init__(self, firstarg, *args):
         # TODO: not sure if np.array is needed, check
+        if isinstance(firstarg, list):
+            x= [p.x for p in firstarg]
+            y = [p.y for p in firstarg]
+            geo_type = firstarg[0].utm_reference
+        else:
+            x, y = args
+            geo_type = firstarg
+        GeoObject.__init__(self, geo_type, x, y)
         xytuple = map(tuple, np.array([self.easting, self.northing]).transpose())
         # print xytuple
         LineString.__init__(self, xytuple)
+
+    def geoEnvelope(self):
+        env_easting, env_northing = np.array(self.envelope.bounds).reshape((2, 2)).transpose()
+        upper_left = GeoPoint(self.utm_reference, env_easting[0], env_northing[1])
+        lower_right = GeoPoint(self.utm_reference, env_easting[1], env_northing[0])
+        return GeoEnvelope(upper_left, lower_right)
+
+
+class GeoEnvelope(GeoPolygon):
+    def __init__(self, upper_left, lower_right):
+        # assum both coordinates are in the same quadrant, choose upper_left by default
+        self.upper_left = upper_left
+        self.lower_right = lower_right
+        GeoPolygon.__init__(self, [upper_left, lower_right])
+
+    def addMargin(self, cartesian_geo_type, margin):
+        # margin is in "units" of cartesian_geo_type, aka if 1m resolution, the one unit of margin
+        # corresponds to one meter of margin
+        upper_left_easting = self.upper_left.easting - margin*cartesian_geo_type.resolution
+        upper_left_northing = self.upper_left.northing + margin*cartesian_geo_type.resolution
+        lower_right_easting = self.lower_right.easting + margin * cartesian_geo_type.resolution
+        lower_right_northing = self.lower_right.northing - margin * cartesian_geo_type.resolution
+        new_upper_left = GeoPoint(self.utm_reference, upper_left_easting, upper_left_northing)
+        new_lower_right =GeoPoint(self.utm_reference, lower_right_easting, lower_right_northing)
+        return GeoEnvelope(new_upper_left, new_lower_right)
+
+    def getBounds(self):
+        return self.upper_left, self.lower_right
+
 
 
 LAT_LONG = LatLon()
