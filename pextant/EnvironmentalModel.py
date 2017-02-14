34669dd700c8786969cc3049afdda346e4e5d363
norheim <norheim@mit.edu>
2017-02-14 16:41:47 -0500

    Transferred relief generation from standalone to within the environmental model

--- pextant/EnvironmentalModel.py
+++ pextant/EnvironmentalModel.py
@@ -1,7 +1,9 @@
 from geoshapely import *
 from osgeo import gdal, osr
 import re
-
+from generate_colormap import colormap
+import shutil
+import docker
 
 class Mesh(object):
     def __init__(self, nw_geo_point, width, height, resolution, dataset, planet='Earth',
@@ -22,6 +24,7 @@ class Mesh(object):
 
 class GDALMesh(Mesh):
     def __init__(self, file_path):
+        self.file_path = file_path
         gdal.UseExceptions()
         dataset = gdal.Open(file_path)
         width = dataset.RasterXSize
@@ -104,22 +107,67 @@ class EnvironmentalModel(Mesh):
     def __init__(self, nw_geo_point, width, height, resolution, dataset, planet, parentMesh=None, xoff=0, yoff=0):
         super(EnvironmentalModel, self).__init__(nw_geo_point, width, height, resolution, dataset, planet,
                                                  parentMesh, xoff, yoff)
-        elevation_map = self.dataset
-        [gx, gy] = np.gradient(elevation_map, resolution, resolution)
-        self.slopes = np.degrees(
-            np.arctan(np.sqrt(np.add(np.square(gx), np.square(gy)))))  # Combining for now for less RAM usage
-        self.numRows, self.numCols = elevation_map.shape
+        self.parent = self.parentMesh
+        self.numRows, self.numCols = self.dataset.shape
         self.obstacles = None  # obstacles is a matrix with boolean values for passable squares
         self.planet = planet
         self.ROW_COL = Cartesian(nw_geo_point, resolution, reverse=True)
         self.special_obstacles = set()  # a list of coordinates of obstacles are not identified by the slope
+
+    def generateRelief(self, N=100):
+        file_dir = 'C:\Users\johan\Dropbox (MIT)\BASALT\pextant\pextant\maps\\'
+        output_dir = 'C:\Users\johan\Desktop\webapp\public\CustomMaps\\'
+
+        regex_result = re.search('.+\/((\w+)\.tif)', self.parent.file_path)
+        file_name = regex_result.group(2)
+        file_extension = regex_result.group(1)
+
+        dataset = self.dataset
+        clean_dataset = dataset[dataset > 0]
+        altitude_lower_bound, altitude_upper_bound = np.min(clean_dataset), np.max(clean_dataset)
+
+        # generate colormap.txt
+        colormap(N, altitude_lower_bound, altitude_upper_bound)
+
+        client = docker.from_env()
+        container = "spara/gdal_ef"
+        volume = {
+            '/c/Users/johan/Dropbox (MIT)/BASALT/pextant/pextant/maps':
+                {'bind': '/data',
+                 'mode': 'rw'},
+        }
+
+        # zoom into area this mesh covers
+        zoom_name = "%s_zoomed.tif" % file_name
+        zoom_command = 'gdal_translate %s %s -srcwin %s %s %s %s' \
+                       % (file_extension, zoom_name, self.xoff, self.yoff, self.width, self.height)
+        client.containers.run(container, zoom_command, volumes=volume)
+
+        # generate relief image tif
+        relief_name = "%s_relief_%s" % (file_name, N)
+        relief_extension = "%s.tif" % relief_name
+        colormap_command = "gdaldem color-relief %s colormap.txt %s -nearest_color_entry" \
+                  % (zoom_name, relief_extension)
+        client.containers.run(container, colormap_command, volumes=volume)
+
+        # tile it!
+        tile_command = "gdal2tiles.py %s" % relief_extension
+        client.containers.run(container, tile_command, volumes=volume)
+
+        from_dir = file_dir + relief_name
+        to_dir = output_dir + relief_name
+        shutil.move(from_dir,
+                    to_dir)
 
     def getGravity(self):
         if self.planet == 'Earth':
             return 9.81
         elif self.planet == 'Moon':
             return 1.622
-
+    def setSlopes(self):
+        [gx, gy] = np.gradient(self.dataset, self.resolution, self.resolution)
+        self.slopes = np.degrees(
+            np.arctan(np.sqrt(np.add(np.square(gx), np.square(gy)))))  # Combining for now for less RAM usage
     def maxSlopeObstacle(self, maxSlope):
         self.obstacles = self.slopes <= maxSlope
         for obstacle in self.special_obstacles:
@@ -150,7 +198,6 @@ class EnvironmentalModel(Mesh):
             for j, item in enumerate(row):
                 if item == noValResult:
                     self.dataset[i][j] = None
-                    self.slopes[i][j] = None
                     self.obstacles[i][j] = True
 
     def _inBounds(self, coordinates):
@@ -172,3 +219,11 @@ class EnvironmentalModel(Mesh):
             return coordinates
         else:
             return coordinates.to(self.ROW_COL)
+
+
+if __name__ == '__main__':
+    from loadWaypoints import loadPoints
+    hi_low = GDALMesh('maps/HI_lowqual_DEM.tif')
+    waypoints = loadPoints('waypoints/HI_13Nov16_MD7_A.json')
+    env_model = hi_low.loadMapSection(waypoints.geoEnvelope())
+    env_model.generateRelief(50)
