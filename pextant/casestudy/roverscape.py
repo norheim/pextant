6b4dbced0219eeb13d5707136ef931a701bdc664
norheim <norheim@mit.edu>
2017-03-13 01:25:53 -0400

    updated to new sextant

--- pextant/casestudy/roverscape.py
+++ pextant/casestudy/roverscape.py
@@ -1,39 +1,30 @@
-from pextant.api import *
-from pextant.EnvironmentalModel import loadElevationMap
-
+from pextant.api import Pathfinder
+from pextant.EnvironmentalModel import GDALMesh
+from pextant.ExplorerModel import Astronaut
+from pextant.lib.geoshapely import GeoEnvelope, GeoPoint, LAT_LONG
 import matplotlib.pyplot as plt
-from osgeo import gdal
+import numpy as np
 
-
-
-dem_map = loadElevationMap('maps/hwmidres.tif', maxSlope = 15,
-                       planet = 'Earth', nw_corner= LatLongCoord(43.4638741,-113.5740501),
-                       se_corner= LatLongCoord(43.4616207432,-113.571586718))
-
+dem_map = GDALMesh('../../data/maps/Ames/Ames.tif')
+env_model = dem_map.loadMapSection()
+plt.matshow(env_model.dataset)
+plt.show()
 astronaut = Astronaut(70)
-P = Pathfinder(astronaut, dem_map)
-#
-# map.convertToRowCol(LatLongCoord(37.41950672542035, -122.06479100044812))
-# map.convertToRowCol(LatLongCoord(37.420130880587934, -122.06522283609952))
-latlong1 = LatLongCoord(43.46329632, -113.57219385)
-utm1 = latLongToUTM(latlong1)
-row, col = dem_map.convertToRowCol(utm1)
+P = Pathfinder(astronaut, env_model)
 
-latlong2 = LatLongCoord(43.46331013, -113.57266771)
-utm2 = latLongToUTM(latlong2)
-row2, col2 = dem_map.convertToRowCol(utm2)
-
-ap1 = ActivityPoint(latlong1, 0)
-ap2 = ActivityPoint(latlong2, 0)
-
-final = P.aStarCompletePath([0, 0, 1], [ap1, ap2], 'tuple')
+waypoint1 = GeoPoint(env_model.ROW_COL,2,2)
+waypoint2 = GeoPoint(env_model.ROW_COL,50,60)
+segmentsout, rawpoints,_ = P.completeSearch('Energy', [waypoint1, waypoint2])
+print segmentsout
+solgrid = np.zeros((env_model.numRows, env_model.numCols))
+for i in rawpoints:
+    solgrid[i] = 1
+plt.matshow(solgrid)
+plt.show()
 # print final
 # path_x = [point[0] for point in final[0]]
 # path_y = [point[1] for point in final[0]]
 #
-# ds = gdal.Open('Ames.tif')
-# band = ds.GetRasterBand(1)
-# arr = band.ReadAsArray()
 #
 # plt.plot([149,71], [179,42], 'ro')
 # plt.imshow(arr, cmap = 'viridis')
