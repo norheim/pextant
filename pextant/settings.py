17dfabc9577600c9a1b1343d9906de8257142cc9
norheim <norheim@mit.edu>
2017-03-14 17:54:47 -0400

    bundle up common directory names in one place

---
+++ pextant/settings.py
@@ -0,0 +1,7 @@
+import os
+DATA_ROOT = os.path.join('..','..','data')
+MAPS_ROOT = os.path.join(DATA_ROOT,'maps')
+WAYPOINTS_ROOT = os.path.join(DATA_ROOT,'waypoints')
+AMES_DEM = os.path.join(MAPS_ROOT, 'Ames', 'Ames.tif')
+HI_DEM = os.path.join(MAPS_ROOT,'test5-DEM.tif')
+TEST_JSON = os.path.join(WAYPOINTS_ROOT, 'HI_13Nov16_MD7_A.json')
