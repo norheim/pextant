2d796aa74f819bf771b7894bcb0f1efa86c7e6c6
norheim <norheim@mit.edu>
2017-03-20 09:39:48 -0400

    debugging for xgds

--- pextant/analysis/loadWaypoints.py
+++ pextant/analysis/loadWaypoints.py
@@ -33,6 +33,7 @@ class JSONloader:
             return cls(data_file.read(), fullfilename)
 
     def get_waypoints(self):
+        print self.jsondata['sequence']
         ways_and_segments = self.jsondata['sequence']
         s = pd.DataFrame(ways_and_segments)
         waypoints = s[s['type'] == 'Station']['geometry']
