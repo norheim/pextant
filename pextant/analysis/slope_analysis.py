0139fcae8ad9fe540be652fc2f88373297b7ef6c
norheim <norheim@mit.edu>
2017-04-04 10:04:57 -0400

    indices of traverses from Hawaii mission days

---
+++ pextant/analysis/slope_analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+# Found manually doing using import gps notebook
+traverses = {
+    'MD2': np.array([[4509,5088],[5937,6516],[10850,11030],[13530,14110]]),
+    'MD3': np.array([[3400,4660],[5010,5500],[18100,18393]]),
+    'MD4': np.array([[2840,4034],[13150,13829]]),
+    'MD5': np.array([[3500,5000]]),
+    'MD6': np.array([[3990,5500]]),
+    'MD7': np.array([[4200,5700]]),
+    'MD8': np.array([[4600,6500],[10400,11000]]),
+    'MD9': np.array([[7900,9600]])
+}
