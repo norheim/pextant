4fcb5125bc82a04e58a395073325609df49a7441
norheim <norheim@mit.edu>
2017-04-25 19:00:49 -0400

    adapt new mesh abstraction

--- pextant/EnvironmentalModel.py
+++ pextant/EnvironmentalModel.py
@@ -1,12 +1,25 @@
 import re
-from pextant.lib.geoshapely import *
-from pextant.lib.geoutils import filled_circle
-from pextant.MeshModel import Mesh, MeshElement, MeshCollection, SearchKernel
 
-from osgeo import gdal, osr
 import numpy.ma as ma
+from osgeo import gdal, osr
 
-class GDALMesh(Mesh):
+from pextant.lib.geoshapely import *
+from pextant.lib.geoutils import filled_grid_circle
+from pextant.mesh.abstractmesh import GridMesh, EnvironmentalModel, SearchKernel, coordinate_transform
+from pextant.mesh.abstractcomponents import MeshCollection
+from pextant.mesh.concretecomponents import MeshElement
+
+class GDALDataset(object):
+    '''
+    This is a wrapper that gives gdal datasets a shape property, similar to numpy arrays
+    '''
+    def __init__(self, dataset, row_size, col_size):
+        self.shape = (row_size, col_size)
+        self.size = row_size*col_size
+        self.raster = dataset
+
+
+class GDALMesh(GridMesh):
     """
     This class should be used for loading all GDAL terrains, and any subset thereof
     """
@@ -15,13 +28,14 @@ class GDALMesh(Mesh):
         self.file_path = file_path
         gdal.UseExceptions()
         dataset = gdal.Open(file_path)
-        width = dataset.RasterXSize
-        height = dataset.RasterYSize
+        x_size = dataset.RasterXSize
+        y_size = dataset.RasterYSize
 
         dataset_info = dataset.GetGeoTransform()
         nw_easting = dataset_info[0]
         nw_northing = dataset_info[3]
         resolution = dataset_info[1]
+        dataset_wrapped = GDALDataset(dataset, row_size=y_size, col_size=x_size)
 
         proj = dataset.GetProjection()
         srs = osr.SpatialReference(wkt=proj)
@@ -30,22 +44,20 @@ class GDALMesh(Mesh):
         zone_number = regex_result.group(1)  # zone letter is group(2)
         nw_geo_point = GeoPoint(UTM(zone_number), nw_easting, nw_northing)
 
-        super(GDALMesh, self).__init__(nw_geo_point, dataset, resolution, width=width, height=height)
+        super(GDALMesh, self).__init__(nw_geo_point, dataset_wrapped, resolution)
 
-    def loadMapSection(self, geo_envelope=None, maxSlope=35, desired_res=None):
+    def _loadSubSection(self, geo_envelope=None, desired_res=None):
         """
-
-
         :param geo_envelope:
         :type geo_envelope pextant.geoshapely.GeoEnvelope
         :param desired_res:
         :return:
         """
         map_nw_corner, zone = self.nw_geo_point, self.nw_geo_point.utm_reference.proj_param["zone"]
-        dataset = self.dataset
+        dataset = self.dataset.raster
 
-        XY = Cartesian(map_nw_corner, self.resolution)
-        map_se_corner = GeoPoint(XY, self.width, self.height)
+        COL_ROW = Cartesian(map_nw_corner, self.resolution)
+        map_se_corner = GeoPoint(COL_ROW, self.x_size, self.y_size)
 
         if geo_envelope is not None:
             selection_nw_corner, selection_se_corner = geo_envelope.getBounds()
@@ -58,7 +70,7 @@ class GDALMesh(Mesh):
 
         inter_easting, inter_northing = np.array(intersection_box.bounds).reshape((2, 2)).transpose()
         intersection_box_geo = GeoPolygon(UTM(zone), inter_easting, inter_northing)  # could change to UTM_AUTO later
-        inter_x, inter_y = intersection_box_geo.to(XY)
+        inter_x, inter_y = intersection_box_geo.to(COL_ROW)
 
         x_offset, max_x = inter_x
         max_y, y_offset = inter_y
@@ -79,144 +91,105 @@ class GDALMesh(Mesh):
 
         band = dataset.GetRasterBand(1)
         map_array = band.ReadAsArray(x_offset, y_offset, x_size, y_size, buf_x, buf_y).astype(np.float)
+        dataset_clean = ma.masked_array(map_array, np.isnan(map_array)).filled(-99999)
+        dataset_clean = ma.masked_array(dataset_clean, dataset_clean < 0)
 
         # TODO: this hack needs explanation
-        nw_coord_hack = GeoPoint(UTM(zone), inter_easting.min(), inter_northing.max()).to(XY)
-        nw_coord = GeoPoint(XY, nw_coord_hack[0], nw_coord_hack[1])
-        return EnvironmentalModel(nw_coord, map_array, desired_res, self.planet,
-                                  maxSlope, self, x_offset, y_offset)
+        nw_coord_hack = GeoPoint(UTM(zone), inter_easting.min(), inter_northing.max()).to(COL_ROW)
+        nw_coord = GeoPoint(COL_ROW, nw_coord_hack[0], nw_coord_hack[1])
+        return (dataset_clean, desired_res, nw_coord, x_offset, y_offset)
+
+    def loadSubSection(self, geo_envelope=None, desired_res=None, **kwargs):
+        #kwargs is reserved for max slope argument
+        (map_array, desired_res, nw_coord, x_offset, y_offset) = self._loadSubSection(geo_envelope, desired_res)
+        return GridMeshModel(nw_coord, map_array, desired_res,
+                             parent_mesh=self, xoff=x_offset, yoff=y_offset, **kwargs)
 
 
-class EnvironmentalModel(Mesh):
-    """
-    This class ultimately represents an elevation map + all of the traversable spots on it.
-    """
-
-    def __init__(self, nw_geo_point, dataset, resolution, planet='Earth', maxSlope=35, parentMesh=None, xoff=0, yoff=0):
-        dataset_clean = ma.masked_array(dataset, np.isnan(dataset)).filled(-99999)
-        dataset_clean = ma.masked_array(dataset_clean, dataset_clean < 0)
-        super(EnvironmentalModel, self).__init__(nw_geo_point, dataset_clean, resolution, planet,
-                                                 parentMesh, xoff, yoff)
-        self.numRows, self.numCols = dataset.shape
-        self.slopes = None
-        self.ismissingdata = self.dataset.mask
-        #TODO: should obstacles be a set? since its a sparse matrix
-        self.obstacles = np.zeros(dataset.shape, dtype=bool)  # obstacles is a matrix with boolean values for passable squares
-        self.special_obstacles = set()  # a list of coordinates of obstacles are not identified by the slope
+class GridMeshModel(EnvironmentalModel):
+    def __init__(self, *arg, **kwargs):
+        super(GridMeshModel, self).__init__(*arg, **kwargs)
         self.searchKernel = SearchKernel()
-        self.setSlopes()
-        self.maxSlope = maxSlope
-        #TODO: make max slope a once only argument (right now it gets passed along several times)
-        self.maxSlopeObstacle(maxSlope)
+        self.ROW_COL = Cartesian(self.nw_geo_point, self.resolution, reverse=True)
+        self.COL_ROW = Cartesian(self.nw_geo_point, self.resolution)
 
-    def getMeshElement(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        return self._getMeshElement(row, col)
-
-    def _getMeshElement(self, row, col):
-        if self._inBounds(row, col):
-            return MeshElement(row, col, self)
+    def _getMeshElement(self, mesh_coordinates):
+        if self._inBounds(mesh_coordinates):
+            #TODO: need to make this a function:
+            geo_coordinates = mesh_coordinates[::-1]*self.resolution
+            return MeshElement(self, mesh_coordinates, geo_coordinates)
         else:
-            raise IndexError("The location (%s, %s) is out of bounds" % row, col)
+            raise IndexError("The location (%s, %s) is out of bounds" %
+                             (mesh_coordinates[0], mesh_coordinates[1]))
 
-    def getNeighbours(self, mesh_element):
-        return self._getNeighbours(mesh_element.getCoordinates())
-
-    def _getNeighbours(self, rowcol):
-        state = np.array(rowcol)
+    def _getNeighbours(self, mesh_coordinates):
+        state = np.array(mesh_coordinates)
         kernel = self.searchKernel
         offset = kernel.getKernel()
         potential_neighbours = offset + state
         passable_neighbours = np.array(filter(self._isPassable, potential_neighbours))
-        return MeshCollection(self, passable_neighbours)
-
-    def getGravity(self):
-        if self.planet == 'Earth':
-            return 9.81
-        elif self.planet == 'Moon':
-            return 1.622
+        # TODO: need to make this a function:
+        coordsxy =  np.fliplr(passable_neighbours)*self.resolution
+        return MeshCollection(self, passable_neighbours.transpose(), coordsxy.transpose())
 
     def setSlopes(self):
         [gx, gy] = np.gradient(self.dataset, self.resolution, self.resolution)
         self.slopes = np.degrees(
             np.arctan(np.sqrt(np.add(np.square(gx), np.square(gy)))))  # Combining for now for less RAM usage
 
-    def maxSlopeObstacle(self, maxSlope):
-        self.obstacles = self.slopes <= maxSlope
-        for obstacle in self.special_obstacles:
-            self.setObstacle(obstacle)  # "special obstacles" still should not be traversable
-
-    def setObstacle(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)  # of the form (row, column)
-        self.obstacles[row][col] = False
-        if coordinates not in self.special_obstacles:
-            self.special_obstacles.add(coordinates)
-
     def setRadialKeepOutZone(self, center, radius):
-        circlex, circley = filled_circle(self.ROW_COL, center, radius)
-        self.obstacles[circlex, circley] = 1
+        circlex, circley = filled_grid_circle(radius)
+        self.obstacles[center+circlex, center+circley] = 1
 
-    def eraseObstacle(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        self.obstacles[row][col] = True
-        if coordinates in self.special_obstacles:
-            self.special_obstacles.remove(coordinates)
+    def getElevations(self, mesh_coordinates):
+        row, col = mesh_coordinates
+        return self.dataset[row,col]
 
-    def getElevation(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        return self.dataset[row][col]
-
+    @coordinate_transform
     def getSlope(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        return self.slopes[row][col]
+        row, col = coordinates
+        return self.slopes[row,col]
 
-    def in_bounds(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        return self._inBounds(row, col)
-
-    def has_data(self, coordinates):
-        row, col = self.convertToRowCol(coordinates)
-        return self._hasdata(row, col)
-
-    def _inBounds(self, row, col):
+    def _inBounds(self, coordinates):
         # determines if a coordinate is within the boundaries of the environmental model
-        return (0 <= row < self.numRows) and (0 <= col < self.numCols)
+        row, col = coordinates
+        return (0 <= row < self.y_size) and (0 <= col < self.x_size)
 
-    def _hasdata(self, row, col):
-        return self._inBounds(row, col) and not self.ismissingdata[row, col] #use lazy evaluation in case out of bounds
+    def _hasdata(self, coordinates):
+        row, col = coordinates
+        return self._inBounds(coordinates) and not self.ismissingdata[row, col]
 
-    def _isPassable(self, rowcol):
-        row, col = rowcol
-        return self._hasdata(row,col) and self.obstacles[row, col]
+    def maxSlopeObstacle(self, maxSlope):
+        self.obstacles = self.slopes > maxSlope
 
-    def isPassable(self, coordinates):
-        # determines if coordinates can be passed through
-        row, col = self.convertToRowCol(coordinates)
-        return self._isPassable((row, col))
+    def _isPassable(self, mesh_coordinates):
+        row, col = mesh_coordinates
+        return self._hasdata(mesh_coordinates) and not self.obstacles[row, col]
 
-    def convertToRowCol(self, coordinates):
+    def convert_coordinates(self, coordinates):
         if isinstance(coordinates, GeoPoint):
             return coordinates.to(self.ROW_COL)
-        else:
+        elif isinstance(coordinates, np.ndarray):
             return coordinates
+        else:
+            return np.array(coordinates)
 
     def cache_neighbours(self):
         s = dict()
-        for i in range(self.numRows):
-            for j in range(self.numCols):
-                s[(i,j)] = self._getNeighbours((i,j))
+        for i in range(self.y_size):
+            for j in range(self.x_size):
+                s[(i,j)] = self.getNeighbours((i,j))
         return s
+
 
 def loadElevationMap(fullPath, maxSlope=35, nw_corner=None, se_corner=None, desiredRes=None):
     geoenvelope = GeoEnvelope(nw_corner, se_corner)
     maxSlope=35 #TODO: need to fix this
     dem = GDALMesh(fullPath)
-    return dem.loadMapSection(geoenvelope, maxSlope=maxSlope, desired_res=desiredRes)
+    return dem.loadSubSection(geoenvelope, maxSlope=maxSlope, desired_res=desiredRes)
 
 
 if __name__ == '__main__':
-    from pextant.settings import AMES_DEM
-    import cProfile
-    ames_em = GDALMesh('../data/maps/Ames/Ames.tif').loadMapSection()
-    cProfile.run('ames_em.cache_neighbours()')
-    print(ames_em.numCols*ames_em.numRows)
+    ames_em = GDALMesh('../data/maps/Ames/Ames.tif').loadSubSection()
+    #cProfile.run('ames_em.cache_neighbours()')
+    print(ames_em.x_size * ames_em.y_size)
