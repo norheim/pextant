d33d191449d2ef9130f3a0c5daf4a9ab18049d92
norheim <norheim@mit.edu>
2017-04-25 18:57:12 -0400

    add more dependencies for the notebook

--- pextant/notebooks/sextant_dependencies.py
+++ pextant/notebooks/sextant_dependencies.py
@@ -5,9 +5,7 @@ import datetime
 import numpy as np
 from pextant.settings import *
 from pextant.analysis.loadWaypoints import JSONloader, sextant_loader
-from pextant.lib.geoshapely import GeoPolygon, GeoPoint, UTM, LAT_LONG, Cartesian, LONG_LAT
 from pextant.EnvironmentalModel import GDALMesh
 from pextant.ExplorerModel import Astronaut
-from pextant.MeshVisualizer import MeshViz, MeshVizM, ExpandViz
-from pextant.solvers.astarSEXTANT import search, fullSearch, ExplorerCost
+from pextant.mesh.MeshVisualizer import MeshViz, MeshVizM, ExpandViz
 import pandas as pd
