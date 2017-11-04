e78f474dd96cb56a7d4ebdcb4e54bcf8b6c0bc70
norheim <norheim@mit.edu>
2017-11-03 20:02:59 -1000

    helper function for decoding waypoints

--- pextant/lib/utils.py
+++ pextant/lib/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pykml import parser
-from geoshapely import GeoPolygon, LONG_LAT
+from geoshapely import GeoPolygon, LONG_LAT, GeoPoint
 import re
 
 def gridpoints_list(array):
@@ -18,3 +18,18 @@ def kml2shapely(kmlfile):
     coordarray = np.array(allcoords, dtype=float).reshape(len(allcoords)/3, 3)[:,:2]
     ring = GeoPolygon(LONG_LAT, *coordarray.transpose())
     return ring
+
+def kmlpoints(kmlfile):
+    with open(kmlfile) as f:
+        doc = parser.parse(f)
+    root = doc.getroot()
+    points = [elt.Point for elt in root.Document.getchildren() if hasattr(elt, "Point")]
+    ordered_waypoints = points[::-1]
+
+    def generateGeoPoint(p):
+        latlongcoord = np.array(str(p.coordinates).split(',')[:2], dtype=float)
+        return GeoPoint(LONG_LAT, *latlongcoord)
+
+    waypoints = GeoPolygon(map(generateGeoPoint, ordered_waypoints))
+
+    return  waypoints
